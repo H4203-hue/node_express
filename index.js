ce16df91af93cc2975637fd1a4a111a74f337f04
Hiiski <henriqueslima09@gmail.com>
2023-11-09 07:59:18 -0300

    commit2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
+const caminhoBase = path.join(__dirname, "templates")
+
 app.get('/', (requisicao, resposta) => {
     resposta.send("Estou funcionando com express")
+    console.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
