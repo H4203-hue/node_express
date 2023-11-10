d2efc380b5534d8b6fd5822da765d52fd3e87fb9
Hiiski <henriqueslima09@gmail.com>
2023-11-10 09:41:20 -0300

    commit Final

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const app = express()
 const caminhoBase = path.join(__dirname, "templates")
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Estou funcionando com express")
     console.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
     console.log("servidor rodando na portal 3000!")
 })
-""
